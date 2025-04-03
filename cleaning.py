f5ee3e514738e8a0584c03aca34c634c90ea620c
amyints <154259348+amyints@users.noreply.github.com>
2025-04-03 14:16:24 -0400

    Modify cleaning.py to create method

--- cleaning.py
+++ cleaning.py
@@ -2,36 +2,47 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-from scipy import stats
-from scipy.stats import pearsonr
 
-# Load the dataset
-df = pd.read_csv("./data/agriculture_dataset.csv")
-clean_df = df.copy()
 
-# Drop missing values
-clean_df.dropna(inplace=True)
-print(f"Missing Values: \n{clean_df.isna().sum()}")
 
-# Drop duplicates
-clean_df.drop_duplicates(inplace=True)
+def data_cleaning(df_path):
 
-# Find outliers
-clean_df['z_score'] = np.abs(stats.zscore(clean_df['Yield(tons)']))
-outliers = clean_df[clean_df['z_score'] > 3]
-print(f"Number of outliers detected: {outliers.shape[0]}")
+    # Load the dataset
+    df = pd.read_csv(df_path)
+    clean_df = df.copy()
 
-# Remove Outliers
-clean_df = clean_df[clean_df['z_score'] <= 3]
+    # Drop missing values
+    clean_df.dropna(inplace=True)
+    print(f"Missing Values: \n{clean_df.isna().sum()}")
 
-# Categorical Encoding
-clean_df = pd.get_dummies(clean_df, columns=['Irrigation_Type', 'Soil_Type'])
+    # Drop duplicates
+    clean_df.drop_duplicates(inplace=True)
 
-# Create a new column for the Fertilizer Usage to Crop Yield ratio
-clean_df['fertilizer_to_yield_ratio'] = clean_df['Fertilizer_Used(tons)'] / clean_df['Yield(tons)']
-clean_df['pesticide_to_yield_ration'] = clean_df['Pesticide_Used(kg)'] / clean_df['Yield(tons)']
+    # Find outliers
+    clean_df['z_score'] = np.abs(stats.zscore(clean_df['Yield(tons)']))
+    outliers = clean_df[clean_df['z_score'] > 3]
+    print(f"Number of outliers detected: {outliers.shape[0]}")
 
-# Select top 3 crops
-popular_by_yield = clean_df.groupby('Crop_Type')['Yield(tons)'].sum().sort_values(ascending=False)
-top_3_crops = popular_by_yield.head(3).index.tolist()
-print(f"Top 3 crops by yield: {top_3_crops}")
+    # Remove Outliers
+    clean_df = clean_df[clean_df['z_score'] <= 3]
+
+    # Categorical Encoding
+    clean_df = pd.get_dummies(clean_df, columns=['Irrigation_Type', 'Soil_Type'])
+
+    # Create a new column for the Fertilizer Usage to Crop Yield ratio
+    clean_df['fertilizer_to_yield_ratio'] = clean_df['Fertilizer_Used(tons)'] / clean_df['Yield(tons)']
+    clean_df['pesticide_to_yield_ratio'] = clean_df['Pesticide_Used(kg)'] / clean_df['Yield(tons)']
+
+    # Select top 3 crops
+    popular_by_yield = clean_df.groupby('Crop_Type')['Yield(tons)'].sum().sort_values(ascending=False)
+    top_3_crops = popular_by_yield.head(3).index.tolist()
+    print(f"Top 3 crops by yield: {top_3_crops}")
+
+    return clean_df, top_3_crops
+
+if __name__ == "__main__":
+    df_path = "./data/agriculture_dataset.csv"
+    clean_df, top_3_crops = data_cleaning(df_path)
+    print(f"Top 3 crops by yield: {top_3_crops}")
+
+    clean_df.to_csv("./data/cleaned_agriculture_dataset.csv", index=False )
