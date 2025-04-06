c028245a8282c3454517ea501170a56bdaba807f
amyints <154259348+amyints@users.noreply.github.com>
2025-04-06 13:57:16 -0400

    update main.py with encoded_df variable

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from analysis import fertilizer_correlation, pesticide_correlation, water_correl
 
 # clean data using cleaning.py
 df_path = "./data/agriculture_dataset.csv"
-clean_df, top_3_crops = data_cleaning(df_path)
+clean_df, top_3_crops, encoded_df = data_cleaning(df_path)
 print(f"These top 3 crop will be referenced to analyze: {top_3_crops}")
 clean_df.to_csv("./data/cleaned_agriculture_dataset.csv", index=False )
 
