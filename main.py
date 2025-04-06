bfe9761e7de7cadc55b493a8ca1c91b2ba23ddd8
amyints <154259348+amyints@users.noreply.github.com>
2025-04-05 20:03:16 -0400

    call irrigation_correlation function and soil_correlation function from main.py

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 from cleaning import data_cleaning
-from analysis import fertilizer_correlation, pesticide_correlation, water_correlation
+from analysis import fertilizer_correlation, pesticide_correlation, water_correlation, irrigation_correlation, soil_correlation
 
 # clean data using cleaning.py
 df_path = "./data/agriculture_dataset.csv"
-clean_df, top_5_crops = data_cleaning(df_path)
-print(f"These top 5 crop will be referenced to analyze: {top_5_crops}")
+clean_df, top_3_crops = data_cleaning(df_path)
+print(f"These top 3 crop will be referenced to analyze: {top_3_crops}")
 clean_df.to_csv("./data/cleaned_agriculture_dataset.csv", index=False )
 
 # Fertilizer Correlation and Statistical Significance
 print("---FERTILIZER CORRELATION---\n")
-fertilizer_correlation(clean_df, f"{top_5_crops[0]}")
-fertilizer_correlation(clean_df, f"{top_5_crops[1]}")
-fertilizer_correlation(clean_df, f"{top_5_crops[2]}")
+fertilizer_correlation(clean_df, f"{top_3_crops[0]}")
+fertilizer_correlation(clean_df, f"{top_3_crops[1]}")
+fertilizer_correlation(clean_df, f"{top_3_crops[2]}")
 
 
 # Pesticide Correlation and Statistical Significance
 print("---PESTICIDE CORRELATION---\n")
-pesticide_correlation(clean_df, f"{top_5_crops[0]}")
-pesticide_correlation(clean_df, f"{top_5_crops[1]}")
-pesticide_correlation(clean_df, f"{top_5_crops[2]}")
+pesticide_correlation(clean_df, f"{top_3_crops[0]}")
+pesticide_correlation(clean_df, f"{top_3_crops[1]}")
+pesticide_correlation(clean_df, f"{top_3_crops[2]}")
 
 
 # Water Usage Correlation and Statistical Significance
 print("---WATER CORRELATION---\n")
-water_correlation(clean_df, f"{top_5_crops[0]}")
-water_correlation(clean_df, f"{top_5_crops[1]}")
-water_correlation(clean_df, f"{top_5_crops[2]}")
+water_correlation(clean_df, f"{top_3_crops[0]}")
+water_correlation(clean_df, f"{top_3_crops[1]}")
+water_correlation(clean_df, f"{top_3_crops[2]}")
+
+#print(clean_df.columns.to_list())
+
+
+# Irrigation Type Correlation and Statistical Significance
+irrigation_correlation(clean_df, top_3_crops[0])
+irrigation_correlation(clean_df, top_3_crops[1])
+irrigation_correlation(clean_df, top_3_crops[2])
+
+# Soil Type Correlation and Statistical Significance
+soil_correlation(clean_df, top_3_crops[0])
+soil_correlation(clean_df, top_3_crops[1])
+soil_correlation(clean_df, top_3_crops[2])
