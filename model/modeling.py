f473b01bb3e5cfd9b9bf041b06b36a004f668645
amyints <154259348+amyints@users.noreply.github.com>
2025-04-06 17:06:10 -0400

    update modeling.py with randomforestregressor

--- model/modeling.py
+++ model/modeling.py
@@ -1,23 +1,30 @@
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score, mean_absolute_error, root_mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import numpy as np
 
 def data_modeling(df):
+    # Prepare features and target
     X = df.drop(columns=['Yield(tons)'])
     y = df['Yield(tons)']
 
+    # Split the data into train and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Train the model using Linear Regression
-    model = LinearRegression()
+    # Initialize and train the Random Forest model
+    model = RandomForestRegressor(n_estimator=300, random_state=42)
     model.fit(X_train, y_train)
+
+    # Predictions
     predictions = model.predict(X_test)
+
+    # Metrics
     r2 = r2_score(y_test, predictions)
     mae = mean_absolute_error(y_test, predictions)
-    rsme = root_mean_squared_error(y_test, predictions, squared=False)
+    rmse = np.sqrt(mean_squared_error(y_test, predictions))
 
     print(f"R-Squared: {r2}")
-    print(f"MAE: {mean_absolute_error(y_test, predictions)}")
-    print(f"RSME: {rsme}")
+    print(f"MAE: {mae}")
+    print(f"RMSE: {rmse}")
 
-    return 
+    return model
