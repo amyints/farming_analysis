48c3c0b5fb9f8097870c76abe463b6d810c98530
amyints <154259348+amyints@users.noreply.github.com>
2025-04-06 14:17:04 -0400

    create modeling.py

---
+++ model/modeling.py
@@ -0,0 +1,18 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_absolute_error, root_mean_squared_error
+
+def data_modeling(df):
+    X = df.drop(columns=['Yield(tons)'])
+    y = df['Yield(tons)']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Train the model using Linear Regression
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+
+    print(f"R-Squared: {r2_score(y_test, predictions)}")
+    print(f"MAE: {mean_absolute_error(y_test, predictions)}")
+    print(f"RSME: {root_mean_squared_error(y_test, predictions, squared=False)}")
