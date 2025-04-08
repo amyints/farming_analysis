9cea756eaa1da2416746c1d8f0fd01aa774d87d2
amyints <amyints@wgu.edu>
2025-04-08 19:34:41 -0400

    do not return model from modeling.py

--- model/modeling.py
+++ model/modeling.py
@@ -49,4 +49,4 @@ def data_modeling(df):
     print(f"MAE: {mae:.2f}")
     print(f"RMSE: {rmse:.2f}")
 
-    return model, predictions, X_train, X_test, y_train, y_test
+    return predictions, X_train, X_test, y_train, y_test
