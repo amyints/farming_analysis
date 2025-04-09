dffc359ee43d19dd941ed3b14b3ccde673f83ea5
amyints <amyints@wgu.edu>
2025-04-08 21:12:44 -0400

    modify cleaning.py by flipping formula to calculate ratios

--- cleaning.py
+++ cleaning.py
@@ -31,9 +31,9 @@ def data_cleaning(df_path):
 
 
     # Create a new column for the Fertilizer Usage to Crop Yield ratio
-    clean_df['fertilizer_to_yield_ratio'] = clean_df['Fertilizer_Used(tons)'] / clean_df['Yield(tons)']
-    clean_df['pesticide_to_yield_ratio'] = clean_df['Pesticide_Used(kg)'] / clean_df['Yield(tons)']
-    clean_df['water_to_yield_ratio'] = clean_df['Water_Usage(cubic meters)'] / clean_df['Yield(tons)']
+    clean_df['fertilizer_to_yield_ratio'] = clean_df['Yield(tons)'] / clean_df['Fertilizer_Used(tons)']
+    clean_df['pesticide_to_yield_ratio'] = clean_df['Yield(tons)'] / clean_df['Pesticide_Used(kg)']
+    clean_df['water_to_yield_ratio'] = clean_df['Yield(tons)'] / clean_df['Water_Usage(cubic meters)']
 
 
     # Select top 3 crops
