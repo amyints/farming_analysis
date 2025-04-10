bb17a218c21fb05ed7e67ba114ed88673a9e3146
amyints <amyints@wgu.edu>
2025-04-09 21:03:12 -0400

    add acreage to modeling.py and fine tune parameters

--- model/modeling.py
+++ model/modeling.py
@@ -1,4 +1,4 @@
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from xgboost import XGBRegressor
 from sklearn.neighbors import KNeighborsRegressor
@@ -11,7 +11,17 @@ import pandas as pd
 def data_modeling(df):
 
     # Prepare features and target
-    X = df[['Fertilizer_Used(tons)', 'Pesticide_Used(kg)', 'Water_Usage(cubic meters)', 'fertilizer_to_yield_ratio', 'pesticide_to_yield_ratio', 'water_to_yield_ratio']]
+    X = df[[
+        'Fertilizer_Used(tons)',
+        'Pesticide_Used(kg)',
+        'Water_Usage(cubic meters)',
+        'fertilizer_to_yield_ratio',
+        'pesticide_to_yield_ratio',
+        'water_to_yield_ratio',
+        'fertilizer_to_acre_ratio',
+        'pesticide_to_acre_ratio',
+        'water_to_acre_ratio'
+        ]]
     y = df['Yield(tons)']
 
 
@@ -21,11 +31,28 @@ def data_modeling(df):
 
     # Initialize and train the Random Forest model\
     base_learners = [
-        ('rf', RandomForestRegressor(n_estimators=200, max_depth=5, min_samples_split=5, max_features='sqrt', random_state=42)),
-        ('gb', GradientBoostingRegressor(n_estimators=100, learning_rate=0.3, random_state=42)),
-        ('et', ExtraTreesRegressor(n_estimators=200, random_state=42)),
+        ('rf', RandomForestRegressor(
+            n_estimators=200,
+            max_depth=5,
+            min_samples_split=5,
+            max_features='sqrt',
+            random_state=42
+            )),
+        ('gb', GradientBoostingRegressor(
+            n_estimators=100,
+            learning_rate=0.3,
+            max_depth=5,
+            max_features='sqrt',
+            loss='quantile',
+            alpha=0.1,
+            random_state=42
+            )),
+        ('et', ExtraTreesRegressor(
+            n_estimators=200,
+            random_state=42
+            )),
         ('knn', Pipeline([
-            ('scaler', StandardScaler()),
+            ('scaler', StandardScaler(with_std=True)),
             ('knn', KNeighborsRegressor(n_neighbors=4)),
          ]))
     ]
@@ -36,6 +63,8 @@ def data_modeling(df):
         learning_rate=0.1,
         max_depth=5,
         gamma=0.1,
+        min_child_weight=5,
+        tree_method='exact',
         random_state=42
         )
     
@@ -51,15 +80,6 @@ def data_modeling(df):
 
     predictions = model.predict(X_test)
 
-    """# Feature importance
-    importances = model.feature_importances_
-    feature_names = X.columns
-    importance_df = pd.DataFrame({'Feature': feature_names, 'Importance': importances})
-    importance_df.sort_values(by='Importance', ascending=False, inplace=True)
-
-    print('\n Feature Importance:')
-    print(importance_df.to_string(index=False))
-    """
     r2 = r2_score(y_test, predictions)
     mae = mean_absolute_error(y_test, predictions)
     rmse = np.sqrt(mean_squared_error(y_test, predictions))
